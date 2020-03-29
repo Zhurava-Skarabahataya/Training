74ebf2171fcb539c1ef6464399726abcc8401268
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-03-29 19:49:11 +0300

    added BinaryHeap

--- src/march/practice/StepickAlgorithms/BinaryHeap.java
+++ src/march/practice/StepickAlgorithms/BinaryHeap.java
@@ -1,6 +1,5 @@
 package march.practice.StepickAlgorithms;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
@@ -17,7 +16,6 @@ public class BinaryHeap {
 
     private void run() {
         Scanner sc = new Scanner(System.in);
-        //ArrayList <Long> lonArr = new ArrayList<>();
         int operations = sc.nextInt();
         PriorityQueue <Long> priorityQueue = new PriorityQueue<>(operations, Collections.reverseOrder());
         for (int i = 0; i < operations; i++) {
@@ -26,13 +24,10 @@ public class BinaryHeap {
                 case "Insert" :{
                     long number = Long.parseLong(sc.next());
                     priorityQueue.add(number);
-                    //lonArr.add(number);
                     break;
                 }
                 case "ExtractMax":{
-                    //long max = Collections.max(lonArr);
                     System.out.println(priorityQueue.poll());
-                   // lonArr.remove(max);
                     break;
                 }
             }
