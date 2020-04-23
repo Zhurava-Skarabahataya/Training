bf75f697f9fffbd6ef87dfe30414735e7a3ee16a
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-04-23 14:43:33 +0300

    level 1 completed

--- src/epamOnline/chapter_1/cycles/Task8.java
+++ src/epamOnline/chapter_1/cycles/Task8.java
@@ -1,4 +1,31 @@
 package epamOnline.chapter_1.cycles;
 
+//Даны два числа. Определить цифры, входящие в запись как первого так и второго числа.
+
+import java.util.Scanner;
+
 public class Task8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        while (a!=0){
+            int digit = a%10;
+            checkIfContains(digit , b);
+            a = a/10;
+        }
+    }
+
+    private static void checkIfContains(int digit, int b) {
+        while (b!=0){
+            int myDig = b%10;
+            if (myDig == digit){
+                System.out.println(myDig);
+                break;
+            }
+            b = b/10;
+        }
+    }
+
+
 }
