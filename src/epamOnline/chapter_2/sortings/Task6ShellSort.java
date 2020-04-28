a880dcc075f80b0506508a624c25129f8686a971
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-04-28 20:38:06 +0300

    level 2 Task6ShellSort

--- src/epamOnline/chapter_2/sortings/Task6ShellSort.java
+++ src/epamOnline/chapter_2/sortings/Task6ShellSort.java
@@ -7,8 +7,27 @@ package epamOnline.chapter_2.sortings;
 // Если 1 + ii a a , то производится перестановка и сдвигаются на один элемент назад.
 // Составить алгоритм этой сортировки.
 
+import java.util.Arrays;
+
 public class Task6ShellSort {
     public static void main(String[] args) {
-
+        int[] array = {4, 6, 1, 20, 12, 3};
+        boolean switches = true;
+        while (switches) {
+            switches = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                int a = array[i];
+                for (int j = i + 1; j < array.length; j++) {
+                    if (a > array[j]) {
+                        int temp = array[j];
+                        array[j] = a;
+                        array[i] = temp;
+                        switches = true;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
