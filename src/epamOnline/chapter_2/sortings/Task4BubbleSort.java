1d660ec729ea1748a7ea8fe0b13413559187e322
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-04-28 15:43:08 +0300

    level 2 Task4BubbleSort

--- src/epamOnline/chapter_2/sortings/Task4BubbleSort.java
+++ src/epamOnline/chapter_2/sortings/Task4BubbleSort.java
@@ -7,8 +7,26 @@ package epamOnline.chapter_2.sortings;
 // Так продолжается до тех пор, пока все элементы не станут расположены в порядке возрастания.
 // Составить алгоритм сортировки, подсчитывая при этом количества перестановок.
 
+import java.util.Arrays;
+
 public class Task4BubbleSort {
     public static void main(String[] args) {
-
+        int[] array = {3, 7, 11, 23, 9, 0, -5, 24};
+        boolean ifSwitch = true;
+        int switches = 0;
+        while (ifSwitch){
+            ifSwitch = false;
+            for (int i = 0; i < array.length-1; i++) {
+                if (array[i] > array[i+1]){
+                     int temp = array[i];
+                     array[i] = array[i+1];
+                     array[i+1] = temp;
+                     switches++;
+                     ifSwitch = true;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println("Switches: " + switches);
     }
 }
