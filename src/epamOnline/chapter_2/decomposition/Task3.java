2f9bcb1a7be5fd35c229436262cf89d4314fcaf5
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-04-29 12:53:46 +0300

    level 2 Task3

--- src/epamOnline/chapter_2/decomposition/Task3.java
+++ src/epamOnline/chapter_2/decomposition/Task3.java
@@ -5,6 +5,9 @@ package epamOnline.chapter_2.decomposition;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        double a = 6;
+        double oneTriangle = Math.sqrt(3) * a * a / 4;
+        double hexagonSquare = oneTriangle * 6;
+        System.out.println(hexagonSquare);
     }
 }
