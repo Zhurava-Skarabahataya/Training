78f275e65843070f509797836155e69526a6108a
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-04-29 12:35:15 +0300

    level 2 Task2GCDForFour

--- src/epamOnline/chapter_2/decomposition/Task2GCDForFour.java
+++ src/epamOnline/chapter_2/decomposition/Task2GCDForFour.java
@@ -1,4 +1,19 @@
 package epamOnline.chapter_2.decomposition;
 
+//2. Написать метод(методы) для нахождения наибольшего общего делителя четырех натуральных чисел.
+
 public class Task2GCDForFour {
+    public static void main(String[] args) {
+        int a = 32;
+        int b = 12;
+        int c = 20;
+        int d = 40;
+        int e = findGcd(findGcd(a, b), findGcd(c, d));
+        System.out.println(e);
+    }
+
+
+    private static int findGcd(int a, int b) {
+        return (b == 0) ? a : findGcd(b, a%b);
+    }
 }
---
+++ src/epamOnline/chapter_2/decomposition/Task3.java
@@ -0,0 +1,10 @@
+package epamOnline.chapter_2.decomposition;
+
+//3. Вычислить площадь правильного шестиугольника со стороной а,
+// используя метод вычисления площади треугольника.
+
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+}
