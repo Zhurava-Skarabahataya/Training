da035afb9b046489120da3b186135ecb0f3992c4
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-04-29 12:02:06 +0300

    level 2 Task7 Task8

--- src/epamOnline/chapter_2/sortings/Task7.java
+++ src/epamOnline/chapter_2/sortings/Task7.java
@@ -1,4 +1,43 @@
 package epamOnline.chapter_2.sortings;
 
+//7. Пусть даны две неубывающие последовательности действительных чисел m n b
+// bbиaaa     21 21
+//.
+//Требуется указать те места, на которые нужно вставлять элементы последовательности
+// m bb b   2 1 в первую последовательность так,
+// чтобы новая последовательность оставалась возрастающей.
+//
+
+import java.util.Arrays;
+
 public class Task7 {
+    public static void main(String[] args) {
+        int[] arrayA = {2, 5, 6, 8, 10, 12};
+        int[] arrayB = {0, 1, 3, 5, 8, 15};
+        mergingArrays(arrayA, arrayB);
+    }
+
+    static void mergingArrays ( int[] firstArray, int [] secondArray){
+        int [] result = new int[firstArray.length + secondArray.length];
+        int firstArrIndex = 0;
+        int secondArrIndex = 0;
+        for (int i = 0; i < result.length; i++) {
+            if (firstArrIndex >= firstArray.length && secondArrIndex < secondArray.length){
+                result[i] = secondArray[secondArrIndex++];
+                System.out.print(firstArrIndex+ " ");
+                continue;
+            }
+            if (secondArrIndex >= secondArray.length && firstArrIndex < firstArray.length){
+                result[i] = firstArray[firstArrIndex++];
+                continue;
+            }
+            if (firstArray[firstArrIndex] <= secondArray[secondArrIndex]){
+                result[i] = firstArray[firstArrIndex++];
+            }
+            else {
+                result[i] = secondArray[secondArrIndex++];
+                System.out.print(firstArrIndex + " ");
+            }
+        }
+    }
 }
--- src/epamOnline/chapter_2/sortings/Task8.java
+++ src/epamOnline/chapter_2/sortings/Task8.java
@@ -1,4 +1,52 @@
 package epamOnline.chapter_2.sortings;
 
+//8.Даны дро,( i i q p , - натуральные).
+// Составить программу, которая приводит эти дроби к общему знаменателю
+// и упорядочивает их в порядке возрастания.
+
 public class Task8 {
+    public static void main(String[] args) {
+        int[] p = {3, 1, 6, 15};
+        int[] q = {4, 2, 7, 17};
+        int denominator = findDenominator(q);
+        System.out.println("Общий знаменатель " + denominator);
+        for (int i = 0; i < p.length; i++) {
+            p[i] = p[i] * (denominator / q[i]);
+        }
+        boolean switches = true;
+        while (switches){
+            switches = false;
+            for (int i = 0; i < p.length-1; i++) {
+                if (p[i] > p[i+1]){
+                    int temp = p[i];
+                    p[i] = p[i+1];
+                    p[i+1] = temp;
+                    temp = q[i];
+                    q[i] = q[i+1];
+                    q[i+1] = temp;
+                    switches = true;
+                }
+            }
+        }
+        for (int i = 0; i < p.length; i++) {
+            p[i] = p[i]/(denominator/q[i]);
+            System.out.println(p[i] + "/" + q[i]);
+        }
+    }
+
+    private static int findDenominator(int[] q) {
+        int a = 0;
+        boolean isFound = false;
+        while (!isFound){
+            a++;
+            isFound = true;
+            for (int value : q) {
+                if (a % value != 0) {
+                    isFound = false;
+                    break;
+                }
+            }
+        }
+        return a;
+    }
 }
