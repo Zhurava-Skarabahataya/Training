76170f0b6fad4782b891d158df376f67929d3277
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-05-03 11:24:13 +0300

    level 3 Task1CamelCaseToSnakeCase

---
+++ src/epamOnline/chapter_3/stringAsCharArray/Task1CamelCaseToSnakeCase.java
@@ -0,0 +1,37 @@
+package epamOnline.chapter_3.stringAsCharArray;
+
+//1. Дан массив названий переменных в camelCase. Преобразовать названия в snake_case.
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task1CamelCaseToSnakeCase {
+    public static void main(String[] args) {
+        String[] vars = {"wildCat", "stupidFox", "theGreatestShowman", "oldMcDonaldHasAFarm" };
+        convertToSnakeCase(vars);
+        //convertToSnakeCaseWay2(vars);
+        System.out.println(Arrays.toString(vars));
+    }
+
+    private static void convertToSnakeCase(String[] vars){
+        for (int i = 0; i < vars.length; i++) {
+            char[] var = vars[i].toCharArray();
+            int switches = 0;
+            for (int j = 0; j < var.length; j++) {
+                if (Character.isUpperCase(var[j])){
+                    int point = j;
+                    vars[i] = vars[i].substring(0, point+switches) + "_" + Character.toLowerCase(var[j]) + vars[i].substring(point+1+switches);
+                    switches++;
+                }
+            }
+        }
+    }
+
+    private static void convertToSnakeCaseWay2(String[] vars){
+        String regex = "([a-z])([A-Z]+)";
+        for (int i = 0; i < vars.length; i++) {
+            vars[i] = vars[i].replaceAll(regex, "$1_$2").toLowerCase();
+        }
+    }
+}
