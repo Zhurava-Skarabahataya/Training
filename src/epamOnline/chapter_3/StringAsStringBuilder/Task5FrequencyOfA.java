157cc38e288d553bd20e3446555b6f26d7d604f5
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-05-04 23:22:37 +0300

    level 3 Task5FrequencyOfA way2

--- src/epamOnline/chapter_3/StringAsStringBuilder/Task5FrequencyOfA.java
+++ src/epamOnline/chapter_3/StringAsStringBuilder/Task5FrequencyOfA.java
@@ -8,6 +8,16 @@ import java.util.regex.Pattern;
 public class Task5FrequencyOfA {
     public static void main(String[] args) {
         String myString = "Abraham Abigail is ambient.";
+        countAsWay1(myString);
+        countAsWay2(myString);
+    }
+
+    private static void countAsWay2(String myString) {
+        String changes = myString.replaceAll("[aAаА]", "");
+        System.out.println(myString.length() - changes.length());
+    }
+
+    private static void countAsWay1(String myString) {
         Pattern p = Pattern.compile("[aAаА]");
         Matcher m = p.matcher(myString);
         int counter = 0;
